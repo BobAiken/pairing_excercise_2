1d8b6d352833b8cf19e1ae6e80c0aa2c2f4d1eb5
Robert Aiken <robertsiraaiken@gmail.com>
2022-09-21 17:21:24 -0500

    Add cities

--- friends.js
+++ friends.js
@@ -1,4 +1,7 @@
 var arrays = ["david", "mark", "doug", "bill", "chuck"];
 //console.log(arrays[2]);
 //console.log(arrays[arrays.length - 1]);
-console.log(arrays[0]);
+//console.log(arrays[0]);
+var cities = ["orlando", "seattle", "NewYork City", "Boston", "SanFransico"];
+cities.push("Houston");
+console.log(cities);
